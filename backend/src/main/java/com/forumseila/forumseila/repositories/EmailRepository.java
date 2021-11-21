db87a48fda6e151aa7cbb32eb8ce3f3ea49b70a1
evairlf <evairleitefeldmann@gmail.com>
2021-11-21 11:31:43 -0300

    Last test if success the auto-heroko deploy

--- backend/src/main/java/com/forumseila/forumseila/repositories/EmailRepository.java
+++ backend/src/main/java/com/forumseila/forumseila/repositories/EmailRepository.java
@@ -1,11 +1,11 @@
 package com.forumseila.forumseila.repositories;
 
 import com.forumseila.forumseila.domain.Email;
-import com.forumseila.forumseila.domain.Owner;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+
 import java.util.UUID;
 
 @Repository
